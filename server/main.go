d53896e91c562e626b54df673761951ee22d60f3
Maj Kravos <majk@Majs-MacBook-Pro.local>
2022-10-20 13:37:59 -0500

    add-todo-item api route accepts 'text' parameter

--- server/main.go
+++ server/main.go
@@ -38,8 +38,15 @@ func main() {
 		// instantiate a new TodoItem object
 		todo_item := &TodoItem{}
 
-		// get todo item text from request
-		text := c.Params("text")
+		// destructure payload into struct
+		payload := struct {
+			Text string `json:"text"`
+		}{}
+
+		// parse request body into payload or return error
+		if err := c.BodyParser(&payload); err != nil {
+			return err
+		}
 
 		// check todo_item for errors
 		if err := c.BodyParser(todo_item); err != nil {
@@ -49,7 +56,7 @@ func main() {
 		// generate todo_item ID
 		todo_item.ID = len(todo_items) + 1
 		// add text to item
-		todo_item.Text = text
+		todo_item.Text = payload.Text
 		// add datetime of creation to item
 		todo_item.Created = time.Now().String()
 
@@ -60,5 +67,6 @@ func main() {
 		return c.JSON(todo_items)
 	})
 
+	// listen on port 8081
 	log.Fatal(app.Listen(":8081"))
 }
