aba32886fce9bdef8261c1616a3bf8f14bc894c7
mkravos <mikekra@outlook.com>
2022-10-21 11:39:49 -0500

    styling additions

--- client/src/components/ToDoItem/ToDoItem.js
+++ client/src/components/ToDoItem/ToDoItem.js
@@ -6,7 +6,7 @@ export default function ToDoItem({id, text, created}) {
         <div className="Todo-item">
             <div className="Todo-content">
                 <div className="Todo-text">Text</div>
-                <div className="Todo-datetime">Datetime</div>
+                <div className="Todo-datetime">Created 10/21/2022 at 11:28</div>
             </div>
             <div className="Todo-controls">
                 <button className="Edit-item-btn">Edit</button>
